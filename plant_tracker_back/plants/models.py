42600de0017d2f3ba028a01afac47a95f14c503d
non4me21 <kacper.w21@wp.pl>
2024-04-17 19:09:00 +0200

    starting project changes

--- plant_tracker_back/plants/models.py
+++ plant_tracker_back/plants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Plant(models.Model):
     id = models.AutoField(primary_key=True)
@@ -6,4 +7,16 @@ class Plant(models.Model):
     slug = models.SlugField(unique=True)
     room = models.CharField(max_length=20)
     last_time_watered = models.DateTimeField(null = True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            count = 1
+            while Plant.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
 
