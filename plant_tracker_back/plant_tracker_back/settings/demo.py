a8bb54cbd1dbef351a12338d7c82398639729c1b
non4me21 <kacper.w21@wp.pl>
2024-09-11 09:35:06 +0200

    ser

--- plant_tracker_back/plant_tracker_back/settings/demo.py
+++ plant_tracker_back/plant_tracker_back/settings/demo.py
@@ -9,7 +9,7 @@ CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'planttracker-production.up.railway.app').split(',')
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://planttracker-production.up.railway.app', 'http://planttracker-production.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
